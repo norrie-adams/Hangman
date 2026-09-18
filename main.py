7aaeeca92a88c8894e64b0d8ea544969300f4590
norrie-adams <normanadams087@gmail.com>
2026-09-18 12:40:47 -0400

    added indexing a list for the hangman character

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-def drawHangman():
-    print("Hangman:")
-    print("""
-            -----
-           ( ? ? )
-           (  -  )
-            -----
-              |
-             /|\\
-            / | \\
-           /  |  \\
-             / \\
-            /   \\
-           """)
+from hangman import hangman
 
-drawHangman()
+def main():
+    print(hangman[0])
+    print(hangman[1])
+    print(hangman[2])
+    print(hangman[3])
+    print(hangman[4])
+    print(hangman[5])
+
+if __name__ == "__main__":
+    main()
